2af42dd42e6e5420af066a8caa38923b49c1cb85
Cyber Knight <cyberknight755@gmail.com>
2023-05-04 02:39:10 +0800

    sunny: Inherit usb component
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -287,7 +287,9 @@ TARGET_COMMON_QTI_COMPONENTS += \
     keymaster \
     media \
     perf \
-    telephony
+    telephony \
+    usb \
+    vibrator
 
 # Power
 PRODUCT_PACKAGES += \
@@ -339,21 +341,8 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # USB
 PRODUCT_PACKAGES += \
-    android.hardware.usb@1.0-service
-
-PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.qcom.usb.sh
-
-PRODUCT_SOONG_NAMESPACES += \
-    vendor/qcom/opensource/usb/etc
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
-    frameworks/native/data/etc/android.hardware.usb.host.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.host.xml
-
-# Vibrator
-$(call inherit-product, vendor/qcom/opensource/vibrator/vibrator-vendor-product.mk)
 
 # VNDK
 PRODUCT_EXTRA_VNDK_VERSIONS := 28 29 30

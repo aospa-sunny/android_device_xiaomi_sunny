0eb5a2e7bc4df4df7fa8c9a7ec8de7a36e9db566
Cyber Knight <cyberknight755@gmail.com>
2023-05-03 13:29:27 +0800

    [SQUASH]: Drop erofs, f2fs compression and project id quota
    Revert "mojito: Switch to lz4"
    
    This reverts commit c8e1f0b02c8557ce5c163c284ac2724b951a299a.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    Revert "mojito: Set erofs pcluster size"
    
    This reverts commit cb42825956c10bd7e98921145e739a3eecd17354.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    Revert "mojito: Dont reserve size in partitions"
    
    This reverts commit 0ca692b59374bc753863c96bdeeaec324e2e7900.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    Revert "mojito: Switch to EROFS for relevant partitions"
    
    This reverts commit 58883e61f0a2da01d4167eebe5ce92d278eb37b9.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    Revert "mojito: Switch to LZ4 as F2FS Compression algorithm"
    
    This reverts commit 6a2bd643b711a7f5614f8a30eec1ec8723fbe099.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    Revert "mojito: Fix fsmgr flag for F2FS Compression"
    
    This reverts commit 8ec40f2eabaef4907caaa6d0feb135ae66e11eba.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    Revert "mojito: fstab: Specify ZSTD as F2FS Compression Algorithm"
    
    This reverts commit 2eb8963aa855950297291532f7b4b3d33016494c.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    Revert "mojito: fstab: Support F2FS compressions and garbage collector"
    
    This reverts commit c3c52d18b33b64100df233372367d35927fe0e53.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>
    
    Revert "mojito: Enable Project ID Quota support on userdata"
    
    This reverts commit 075b04a7dfc08b31baf22128e7e70bf1132fc86e.

--- device.mk
+++ device.mk
@@ -10,9 +10,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/developer_gsi_keys.mk)
 # Call the proprietary setup
 $(call inherit-product, vendor/xiaomi/sunny/sunny-vendor.mk)
 
-# Project ID Quota
-$(call inherit-product, $(SRC_TARGET_DIR)/product/emulated_storage.mk)
-
 # A/B
 # Inherit virtual_ab_ota product
 $(call inherit-product, $(SRC_TARGET_DIR)/product/virtual_ab_ota/launch_with_vendor_ramdisk.mk)
@@ -28,13 +25,13 @@ PRODUCT_PACKAGES_DEBUG += \
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_system=true \
     POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=erofs \
+    FILESYSTEM_TYPE_system=ext4 \
     POSTINSTALL_OPTIONAL_system=true
 
 AB_OTA_POSTINSTALL_CONFIG += \
     RUN_POSTINSTALL_vendor=true \
     POSTINSTALL_PATH_vendor=bin/checkpoint_gc \
-    FILESYSTEM_TYPE_vendor=erofs \
+    FILESYSTEM_TYPE_vendor=ext4 \
     POSTINSTALL_OPTIONAL_vendor=true
 
 PRODUCT_PACKAGES += \

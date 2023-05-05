09419e89cee59dbeebace2f20e43c08f98f770c3
Cyber Knight <cyberknight755@gmail.com>
2023-05-06 02:09:34 +0800

    sunny: Import GPS configurations from stock
    - These match our previous in-tree HAL behaviour.
    
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- device.mk
+++ device.mk
@@ -161,11 +161,12 @@ PRODUCT_PACKAGES += \
     FM2 \
     qcom.fmradio
 
-PRODUCT_PACKAGES += \
-    apdr.conf \
-    izat.conf \
-    lowi.conf \
-    sap.conf
+# GPS Configs
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/gps/apdr.conf:$(TARGET_COPY_OUT_VENDOR)/etc/apdr.conf \
+    $(LOCAL_PATH)/configs/gps/izat.conf:$(TARGET_COPY_OUT_VENDOR)/etc/izat.conf \
+    $(LOCAL_PATH)/configs/gps/lowi.conf:$(TARGET_COPY_OUT_VENDOR)/etc/lowi.conf \
+    $(LOCAL_PATH)/configs/gps/sap.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sap.conf
 
 # Health
 PRODUCT_PACKAGES += \

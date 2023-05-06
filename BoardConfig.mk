70281453a3602be6e58e24d23a53af50c26af5ea
Cyber Knight <cyberknight755@gmail.com>
2023-05-07 00:43:17 +0800

    sunny: Enforcing moment
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -111,7 +111,6 @@ BOARD_KERNEL_CMDLINE += service_locator.enable=1
 BOARD_KERNEL_CMDLINE += swiotlb=1
 BOARD_KERNEL_CMDLINE += msm_rtb.filter=0x237
 BOARD_KERNEL_CMDLINE += kpti=off
-BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
 
 BOARD_KERNEL_SEPARATED_DTBO := true
 KERNEL_DEFCONFIG := mojito_defconfig
